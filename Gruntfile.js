30a00a1f4f53329c6f8ff68fed264e839ec09002
Robert Rawlins <robrawlins@gmail.com>
2019-03-28 12:40:42 +0000

    Pulled out node unit as we didn't have any tests in the suite anyway.

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,12 +62,11 @@ module.exports = function(grunt) {
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-release');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'sorry_theme_deploy', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'sorry_theme_deploy']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
